4a7e079971f1fadf66abd5545f7720081d169e10
elzein2807 <elzein2807@gmail.com>
2026-04-06 21:59:07 +0200

    Debug: add error logging to serverless entry

--- api/index.js
+++ api/index.js
@@ -1,2 +1,8 @@
-const app = require('../server/server');
-module.exports = app;
+try {
+  const app = require('../server/server');
+  module.exports = app;
+} catch (err) {
+  module.exports = (req, res) => {
+    res.status(500).json({ error: err.message, stack: err.stack });
+  };
+}
